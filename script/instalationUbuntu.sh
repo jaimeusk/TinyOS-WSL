39ccc2cfec99c6332bd1ed1bb5f9d6f88efb79c9
jaiarnram <jaiarnram@alum.us.es>
2023-03-31 23:21:18 +0200

    Probando nuevo script

--- script/instalationUbuntu.sh
+++ script/instalationUbuntu.sh
@@ -9,9 +9,9 @@ apt-get -y install linux-tools-generic hwdata
 update-alternatives --install /usr/local/bin/usbip usbip /usr/lib/linux-tools/*-generic/usbip 20
 
 apt-get -y install automake emacs bison flex ;\
-    gperf git make openjdk-8-jdk-headless ;\ 
-    gcc-msp430 g++ python3 python3-serial ;\
-    python python-serial 
+apt-get -y gperf git make openjdk-8-jdk-headless ;\ 
+apt-get -y gcc-msp430 g++ python3 python3-serial ;\
+apt-get -y python python-serial 
 
 cd /opt/
 git clone https://github.com/tinyos/nesc.git
