39c061b35c28e674ff9b0cdd0c55bfb9cb2115e8
jaiarnram <jaiarnram@alum.us.es>
2023-03-31 23:39:46 +0200

    ¿FIXED AT THE END?

--- script/instalationUbuntu.sh
+++ script/instalationUbuntu.sh
@@ -8,10 +8,10 @@ echo "Instalando usbip..."
 apt-get -y install linux-tools-generic hwdata
 update-alternatives --install /usr/local/bin/usbip usbip /usr/lib/linux-tools/*-generic/usbip 20
 
-apt-get -y install automake emacs bison flex make
-apt-get -y gperf git make openjdk-8-jdk-headless 
-apt-get -y gcc-msp430 g++ python3 python3-serial
-apt-get -y python python-serial 
+apt-get -y install automake emacs bison flex
+apt-get -y install gperf git make openjdk-8-jdk-headless
+apt-get -y install gcc-msp430 g++ python3 python3-serial
+apt-get -y install python python-serial
 
 cd /opt/
 git clone https://github.com/tinyos/nesc.git
